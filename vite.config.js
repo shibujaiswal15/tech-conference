3659c2da973bee8b67317569e8dccc40510087a7
Shibu <shibu@gmail.com>
2025-02-23 22:57:07 +0530

    assignment

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,9 @@
-// import { sveltekit } from '@sveltejs/kit/vite';
-// import { defineConfig } from 'vite';
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
 
-// export default defineConfig({
-// 	plugins: [sveltekit()]
- //});
-
-
- import { sveltekit } from '@sveltejs/kit/vite';
- import { defineConfig } from 'vite';
- 
- export default defineConfig({
-   plugins: [
-	 sveltekit(),
-   ],
-   server: {
-	 hmr: {
-	   clientPort: 3000, // You can specify a port here if needed
-	 },
-   },
-   build: {
-	 target: 'esnext',
-	 polyfillDynamicImport: false,
-   },
-   optimizeDeps: {
-	 include: ['svelte', '@sveltejs/kit'],
-   },
+export default defineConfig({
+	plugins: [sveltekit()]
  });
+
+
  
